9b81773d123672747a2e5b969e8164e25618cdf8
NhatTuyen00 <83211886+NhatTuyen00@users.noreply.github.com>
2021-06-24 22:56:02 +0700

    update

---
+++ C2/Controllers/RetryLimiExceededException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace C2.Controllers
+{
+    [Serializable]
+    internal class RetryLimiExceededException : Exception
+    {
+        public RetryLimiExceededException()
+        {
+        }
+
+        public RetryLimiExceededException(string message) : base(message)
+        {
+        }
+
+        public RetryLimiExceededException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RetryLimiExceededException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ C2/Controllers/RetryLimiExee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace C2.Controllers
+{
+    [Serializable]
+    internal class RetryLimiExee : Exception
+    {
+        public RetryLimiExee()
+        {
+        }
+
+        public RetryLimiExee(string message) : base(message)
+        {
+        }
+
+        public RetryLimiExee(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RetryLimiExee(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
